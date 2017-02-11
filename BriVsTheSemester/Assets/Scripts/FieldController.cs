f65e608b70cb18326c7466d2ffb8dc4a69c58a20
briblue3 <brianna.hill.3@live.com>
2017-02-11 12:01:36 -0500

    Part-1
    Flying book sprite animated, behavior and generation added.

---
+++ BriVsTheSemester/Assets/Scripts/FieldController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		// Debug.Log ("Collided");
+		if (other.gameObject.CompareTag("Books")) {
+			// destroy flying book if it reaches edge of playing field
+			Destroy(other.gameObject);
+		}
+	}
+}
